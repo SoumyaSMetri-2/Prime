9dacc7142590f0a4779eeded433652811188a2cf
Shivam Yadav <sy7052789@gmail.com>
2020-09-30 19:19:42 +0530

    Added comments in program
    It is good to add comments before each valuable line. Easy to understand for newbies.

--- Prime.java
+++ Prime.java
@@ -1,23 +1,39 @@
-package prime;
+package Prime;
 import java.util.Scanner;
 public class Prime {
+    // this function will print the value such as weather the number is prime or not
     public static void sopln(String str){
         System.out.println(str);
     }
     public static void main(String[] args) {
+        // For taking inputs Scanner calss is must
         Scanner a = new Scanner(System.in);
+        
+        // calling the function *sopln* to print message
         sopln("Please insert any number: ");
+        
+        // calling the function of Scanner class to take an integer input
         int num = a.nextInt();
+        
+        // initializing the default value
         boolean isprime = true;
+
+        // iterating the loop from 2 to the input number
         for(int i = 2; i<num; i++){
-            if(num%i==0){
+        
+            // checking if number is divisble from 2 to *n-1* or not
+            if(num % i == 0) {
+            
+                //if yes, updating the default value 
                 isprime = false;
                 break;
             }
         }
-        if(isprime){
-            sopln(num + " is prime number");
-        }else{
+        
+        // printing the desired messages
+        if(isprime) {
+            sopln(num + " is a prime number");
+        }else {
             sopln(num + " is not a prime number");
         }
     }
